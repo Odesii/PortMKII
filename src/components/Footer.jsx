d16d10fcc4f8e71f31831a5c3aa03871c5e4fbb7
Odesii <alic.madd@gmail.com>
2024-05-24 17:33:59 -0400

    starting file structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+
+
+const Footer = () => (
+  <footer>
+    <a href="https://github.com/Odesii">GitHub</a>
+  </footer>
+);
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+import Navigation from './Navigation';
+
+const Header = () => (
+  <header>
+    <h1>Name</h1>
+    <Navigation />
+  </header>
+);
+
+export default Header;
