06e6830bbeb271d26d8c2c56d99ff06d30e05538
Odesii <alic.madd@gmail.com>
2024-05-25 15:33:08 -0400

    updated tailwind to work with vite and added utils & cosnstants for thew project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React,  {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+import {styles} from '../style';
+import { navLinks } from '../cosnstants';
+import {logo, menu, close} from '../assets';
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center py-y fixed top-o z-20 bg-primary`}>
+      <div className="w-full flex justify-between item-center max-w-7xl mx-auto">
+        <Link 
+          to="/" 
+          className="flex items-center gap-2" 
+          onClick={()=> {
+            setActive("");
+            window.scrollTo(0,0);
+          }}>
+            <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+            <p className=" text-white text-[18-px] font-bold cursor-pointer">| Odesii |</p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) =>(
+            <li>
+              <a href={`#${link.id}>`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
   )
 }
 
