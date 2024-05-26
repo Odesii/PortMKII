a8828cc345d36050f2e7238e6fed61e8181f7ced
Odesii <alic.madd@gmail.com>
2024-05-25 20:40:23 -0400

    hero section 'complete' updated package.json for better performance

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import {Html, useProgress} from '@react-three/drei';
+import { progress } from 'framer-motion';
 
+// this is the loader component that will be shown while the model is loading
 const Loader = () => {
+const {progress} = useProgress()
+
   return (
-    <div>Loader</div>
+<Html>
+  <span className='canvas-loader'></span>
+  <p 
+    style={{
+      fontSize: 15,
+      color: '#f1f1f1',
+      fontWeight: 800,
+      marginTop: 40,
+    }}
+    // show the progress of the loading
+  >{progress.toFixed(2)}%</p>
+</Html>
   )
 }
 
