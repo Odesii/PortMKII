8c5159f16f05a9b3359e9da03f8745c3edacf028
Odesii <alic.madd@gmail.com>
2024-05-27 14:36:05 -0400

    email updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,9 +39,9 @@ const handleSubmit = (e) => {
       import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
       {
         from_name: form.name,
-        to_name: "JavaScript Mastery",
+        to_name: "Odesii",
         from_email: form.email,
-        to_email: "sujata@jsmastery.pro",
+        to_email: import.meta.env.VITE_APP_EMAIL,
         message: form.message,
       },
       import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
