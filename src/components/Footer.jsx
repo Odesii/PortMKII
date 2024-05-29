af75b4fd15a528575869d89223262ce824555f4f
Odesii <alic.madd@gmail.com>
2024-05-28 21:17:54 -0400

    footer section commented

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import {logo, github} from '../assets';
 
-
+// a footer doesnt fit with the style of this website so i will leave it empty
+//also no social media links are provided so i will leave it empty
 const Footer = () => {
 }
 
